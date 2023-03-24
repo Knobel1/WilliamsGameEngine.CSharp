31df9ee7a07b9cc4b98cffc1705a8089e016cfe0
Kody Knobel <knobelko@rsd407.org>
2023-03-24 11:28:02 -0700

    started ship

---
+++ MyGame/Ship.cs
@@ -0,0 +1,31 @@
+﻿using GameEngine;
+using SFML.Graphics;
+using SFML.System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyGame
+{
+    class Ship : GameObject
+    {
+        private readonly Sprite _sprite = new Sprite();
+        // Creates our ship.
+        Ship()
+        {
+            _sprite.Texture = Game.GetTexture("Resources/ship.png");
+            _sprite.Position = new Vector2f(100, 100);
+        }
+        // functions overridden from GameObject:
+        public override void Draw()
+        {
+            Game.RenderWindow.Draw(_sprite);
+        }
+        public override void Update(Time elapsed)
+        {
+            Vector2f pos = _sprite.Position; // had to add something
+        }
+    }
+}
